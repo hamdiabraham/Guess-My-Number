a738eafa70aa3534b389e51ff2aef214736ce92e
hamdiabraham <hamdiabraham.kh@gmail.com>
2021-07-24 18:50:22 +0700

    first comment

---
+++ starter/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".message").textContent = "Correct Number!!";
+
+document.querySelector;
+
+document.querySelector;
