02a54812b9f1f1cc2f3f94fcf7140b3e1152997e
hamdiabraham <hamdiabraham.kh@gmail.com>
2021-07-24 22:03:36 +0700

    logic guess

--- starter/script.js
+++ starter/script.js
@@ -11,10 +11,18 @@ document.querySelector(".check").addEventListener("click", function () {
 
   console.log(guess, typeof guess);
 
+  // if there is no input
   if (!guess) {
     document.querySelector(".message").textContent = "⛔ No number!!";
+
+    // when player win the game
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "✔ Correct number!!";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+
+    document.querySelector(".number").style.width = "30rem";
+
+    // when the input is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📈 Too high!!";
@@ -24,6 +32,8 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "Game over!! You lose!!";
       document.querySelector(".score").textContent = 0;
     }
+
+    // when the input is too high
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📉 Too low!!";
