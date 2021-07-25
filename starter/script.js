9634e7f2412cc8f27cecc27c2ea3480364ddc7db
hamdiabraham <hamdiabraham.kh@gmail.com>
2021-07-25 12:58:53 +0700

    fix bug score

--- starter/script.js
+++ starter/script.js
@@ -1,10 +1,8 @@
 "use strict";
 
-const secretNumber = Math.floor(Math.random() * 20) + 1;
-
+// declare variable secretNumber dan score diluar fungsi memakai let biar bisa di reassign ketika mereset game
+let secretNumber = Math.floor(Math.random() * 20) + 1;
 let score = 20;
-
-document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -18,6 +16,8 @@ document.querySelector(".check").addEventListener("click", function () {
     // when player win the game
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "✔ Correct number!!";
+    document.querySelector(".number").textContent = secretNumber;
+
     document.querySelector("body").style.backgroundColor = "#60b347";
 
     document.querySelector(".number").style.width = "30rem";
@@ -44,4 +44,31 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = 0;
     }
   }
+});
+
+// Coding Challenge #1
+
+/* 
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀
+*/
+
+// function yang apabila di klik akan mengembalikan semua keadaan seperti sebelumnya
+document.querySelector(".again").addEventListener("click", function () {
+  // harus declare kembali score dan secretNumber
+  score = 20;
+  secretNumber = Math.floor(Math.random() * 20) + 1;
+
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
